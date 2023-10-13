567eca9cc72a878615e12231b0f56ddd4fe979f3
farsousa <andersoncaj26@gmail.com>
2023-10-13 15:40:28 -0300

    implementação do desafio

---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends RuntimeException {
+
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
